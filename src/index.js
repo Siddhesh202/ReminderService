9f77c13fdf4b25b27219db553c6c2df41761edaa
Siddhesh202 <siddheshpisal76@gmail.com>
2023-01-11 19:30:45 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('../src/config/serverConfig');
 
-const { sendBasicEmail } = require('../src/services/email-service');
+// const { sendBasicEmail } = require('../src/services/email-service');
+
+const cron = require('node-cron');
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -20,6 +22,10 @@ const setupAndStartServer = async () => {
         //     'This is a testing email',
         //     'Hey, how are you, I hope you like the support'
         // )
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+        });
     });
 }
 
