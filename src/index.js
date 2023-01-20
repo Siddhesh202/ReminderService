177ceef2dc43d4ae2c695357766e81369b92c278
Siddhesh202 <siddheshpisal76@gmail.com>
2023-01-21 01:49:15 +0530

    Added rabbitmq config

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const { PORT } = require('../src/config/serverConfig');
 
 const TicketController = require('./controllers/ticket-controller');
 const jobs = require('./utils/job');
+const { createChannel } = require('./utils/messageQueue');
+const { sendBasicEmail } = require('./services/email-service');
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -14,6 +16,7 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    // const channel = await createChannel();
     app.post('/api/v1/tickets', TicketController.create);
     
     app.listen(PORT, () => {
