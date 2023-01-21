aa95473c79dcb0c28daf6f3ea5ea3700bed6c8d6
Siddhesh202 <siddheshpisal76@gmail.com>
2023-01-21 16:47:17 +0530

    Added some queue related functions

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('../src/config/serverConfig');
 
-// const { sendBasicEmail } = require('../src/services/email-service');
-
 const TicketController = require('./controllers/ticket-controller');
 const jobs = require('./utils/job');
-const { createChannel } = require('./utils/messageQueue');
-const { sendBasicEmail } = require('./services/email-service');
+const { createChannel, subscribeMessage } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig')
+const EmailService = require('./services/email-service');
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -16,19 +15,14 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    // const channel = await createChannel();
+    const channel = await createChannel();
     app.post('/api/v1/tickets', TicketController.create);
+
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
     
     app.listen(PORT, () => {
         console.log(`Server Started on Port: ${PORT}`);
-        jobs();
-        // sendBasicEmail(
-        //     'support@admin.com',
-        //     'scpisal_b20@it.vjti.ac.in',
-        //     'This is a testing email',
-        //     'Hey, how are you, I hope you like the support'
-        // )
-
+        // jobs();
     });
 }
 
--- src/utils/messageQueue.js
+++ src/utils/messageQueue.js
@@ -13,11 +13,13 @@ const createChannel = async () => {
 
 const subscribeMessage = async (channel, service, binding_key) => {
     try {
-        const applicationQueue = await channel.assertQueue('QUEUE_NAME')
+        const applicationQueue = await channel.assertQueue('REMINDER_QUEUE')
         channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
         channel.consume(applicationQueue.queue, msg => {
             console.log('Received data');
             console.log(msg.content.toString());
+            const payload = JSON.parse(msg.content.toString());
+            service(payload);
             channel.ack(msg);
         });
 
@@ -28,7 +30,7 @@ const subscribeMessage = async (channel, service, binding_key) => {
 
 const publishMessage = async (channel, binding_key, message) => {
     try {
-        await channel.assertQueue('QUEUE_NAME')
+        await channel.assertQueue('REMINDER_QUEUE')
         await channel.publish(EXCHANGE_NAME, binding_key, Buffer.from(message));
 
     } catch (error) {
